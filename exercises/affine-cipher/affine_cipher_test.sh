ea40d0b7e534b89e9f95d1036f35f311d8fe4326
Glenn Jackman <jackman@pythian.com>
2020-10-27 19:35:57 -0400

    x

--- exercises/affine-cipher/affine_cipher_test.sh
+++ exercises/affine-cipher/affine_cipher_test.sh
@@ -1,70 +1,97 @@
 #!/usr/bin/env bash
 
 # local version: 2.0.0.0
-
 # encode
+verbose_test_info() {
+  if [[ $BATS_VERBOSE ]] && { (( status != expected[1] )) || [[ $output != "${expected[0]}" ]]; }; then
+    {
+      printf 'test %d: %s\n' "$BATS_TEST_NUMBER" "$BATS_TEST_DESCRIPTION"
+      printf 'status: %d (expected: %d)\n' "$status" "$expected[1]"
+      printf 'expected: "%s"\n' "${expected[0]}"
+      printf 'actual: "%s"\n' "$output"
+    } | sed 's/^/# /' >&3
+  fi
+}
 
- @test "encode yes" {
+@test "encode yes" {
   #[[ $BATS_RUN_SKIPPED == "true" ]] || skip
   run bash affine_cipher.sh encode 5 7 "yes"
-  (( status == 0 ))
-  [[ $output == "xbt" ]]
+  expected=( "xbt" 0 )
+  verbose_test_info
+  (( status == expected[1] ))
+  [[ $output == "${expected[0]}" ]]
 }
 
  @test "encode no" {
   [[ $BATS_RUN_SKIPPED == "true" ]] || skip
   run bash affine_cipher.sh encode 15 18 "no"
-  (( status == 0 ))
-  [[ $output == "fu" ]]
+  expected=( "fu" 0 )
+  verbose_test_info
+  (( status == expected[1] ))
+  [[ $output == "${expected[0]}" ]]
 }
 
  @test "encode OMG" {
   [[ $BATS_RUN_SKIPPED == "true" ]] || skip
   run bash affine_cipher.sh encode 21 3 "OMG"
-  (( status == 0 ))
-  [[ $output == "lvz" ]]
+  expected=( "lvz" 0 )
+  verbose_test_info
+  (( status == expected[1] ))
+  [[ $output == "${expected[0]}" ]]
 }
 
  @test "encode O M G" {
   [[ $BATS_RUN_SKIPPED == "true" ]] || skip
   run bash affine_cipher.sh encode 25 47 "O M G"
-  (( status == 0 ))
-  [[ $output == "hjp" ]]
+  expected=( "hjp" 0 )
+  verbose_test_info
+  (( status == expected[1] ))
+  [[ $output == "${expected[0]}" ]]
 }
 
  @test "encode mindblowingly" {
   [[ $BATS_RUN_SKIPPED == "true" ]] || skip
   run bash affine_cipher.sh encode 11 15 "mindblowingly"
-  (( status == 0 ))
-  [[ $output == "rzcwa gnxzc dgt" ]]
+  expected=( "rzcwa gnxzc dgt" 0 )
+  verbose_test_info
+  (( status == expected[1] ))
+  [[ $output == "${expected[0]}" ]]
 }
 
  @test "encode numbers" {
   [[ $BATS_RUN_SKIPPED == "true" ]] || skip
   run bash affine_cipher.sh encode 3 4 "Testing,1 2 3, testing."
-  (( status == 0 ))
-  [[ $output == "jqgjc rw123 jqgjc rw" ]]
+  expected=( "jqgjc rw123 jqgjc rw" 0 )
+  verbose_test_info
+  (( status == expected[1] ))
+  [[ $output == "${expected[0]}" ]]
 }
 
  @test "encode deep thought" {
   [[ $BATS_RUN_SKIPPED == "true" ]] || skip
   run bash affine_cipher.sh encode 5 17 "Truth is fiction."
-  (( status == 0 ))
-  [[ $output == "iynia fdqfb ifje" ]]
+  expected=( "iynia fdqfb ifje" 0 )
+  verbose_test_info
+  (( status == expected[1] ))
+  [[ $output == "${expected[0]}" ]]
 }
 
  @test "encode all the letters" {
   [[ $BATS_RUN_SKIPPED == "true" ]] || skip
   run bash affine_cipher.sh encode 17 33 "The quick brown fox jumps over the lazy dog."
-  (( status == 0 ))
-  [[ $output == "swxtj npvyk lruol iejdc blaxk swxmh qzglf" ]]
+  expected=( "swxtj npvyk lruol iejdc blaxk swxmh qzglf" 0 )
+  verbose_test_info
+  (( status == expected[1] ))
+  [[ $output == "${expected[0]}" ]]
 }
 
  @test "encode with a not coprime to m" {
   [[ $BATS_RUN_SKIPPED == "true" ]] || skip
   run bash affine_cipher.sh encode 6 17 "This is a test."
-  (( status == 1 ))
-  [[ $output == "a and m must be coprime." ]]
+  expected=( "a and m must be coprime." 1 )
+  verbose_test_info
+  (( status == expected[1] ))
+  [[ $output == "${expected[0]}" ]]
 }
 
 # decode
@@ -72,48 +99,62 @@
  @test "decode exercism" {
   [[ $BATS_RUN_SKIPPED == "true" ]] || skip
   run bash affine_cipher.sh decode 3 7 "tytgn fjr"
-  (( status == 0 ))
-  [[ $output == "exercism" ]]
+  expected=( "exercism" 0 )
+  verbose_test_info
+  (( status == expected[1] ))
+  [[ $output == "${expected[0]}" ]]
 }
 
  @test "decode a sentence" {
   [[ $BATS_RUN_SKIPPED == "true" ]] || skip
   run bash affine_cipher.sh decode 19 16 "qdwju nqcro muwhn odqun oppmd aunwd o"
-  (( status == 0 ))
-  [[ $output == "anobstacleisoftenasteppingstone" ]]
+  expected=( "anobstacleisoftenasteppingstone" 0 )
+  verbose_test_info
+  (( status == expected[1] ))
+  [[ $output == "${expected[0]}" ]]
 }
 
  @test "decode numbers" {
   [[ $BATS_RUN_SKIPPED == "true" ]] || skip
   run bash affine_cipher.sh decode 25 7 "odpoz ub123 odpoz ub"
-  (( status == 0 ))
-  [[ $output == "testing123testing" ]]
+  expected=( "testing123testing" 0 )
+  verbose_test_info
+  (( status == expected[1] ))
+  [[ $output == "${expected[0]}" ]]
 }
 
  @test "decode all the letters" {
   [[ $BATS_RUN_SKIPPED == "true" ]] || skip
   run bash affine_cipher.sh decode 17 33 "swxtj npvyk lruol iejdc blaxk swxmh qzglf"
-  (( status == 0 ))
-  [[ $output == "thequickbrownfoxjumpsoverthelazydog" ]]
+  expected=( "thequickbrownfoxjumpsoverthelazydog" 0 )
+  verbose_test_info
+  (( status == expected[1] ))
+  [[ $output == "${expected[0]}" ]]
 }
 
  @test "decode with no spaces in input" {
   [[ $BATS_RUN_SKIPPED == "true" ]] || skip
   run bash affine_cipher.sh decode 17 33 "swxtjnpvyklruoliejdcblaxkswxmhqzglf"
-  (( status == 0 ))
-  [[ $output == "thequickbrownfoxjumpsoverthelazydog" ]]
+  expected=( "thequickbrownfoxjumpsoverthelazydog" 0 )
+  verbose_test_info
+  (( status == expected[1] ))
+  [[ $output == "${expected[0]}" ]]
 }
 
  @test "decode with too many spaces" {
   [[ $BATS_RUN_SKIPPED == "true" ]] || skip
   run bash affine_cipher.sh decode 15 16 "vszzm    cly   yd cg    qdp"
-  (( status == 0 ))
-  [[ $output == "jollygreengiant" ]]
+  expected=( "jollygreengiant" 0 )
+  verbose_test_info
+  (( status == expected[1] ))
+  [[ $output == "${expected[0]}" ]]
 }
 
  @test "decode with a not coprime to m" {
   [[ $BATS_RUN_SKIPPED == "true" ]] || skip
   run bash affine_cipher.sh decode 13 5 "Test"
-  (( status == 1 ))
-  [[ $output == "a and m must be coprime." ]]
+  expected=( "a and m must be coprime." 1 )
+  verbose_test_info
+  (( status == expected[1] ))
+  [[ $output == "${expected[0]}" ]]
 }
